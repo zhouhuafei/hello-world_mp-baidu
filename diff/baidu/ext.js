bc2b513d40c23e93c941daa78dbd11d87b6b7ded
zhouhuafei <1123486116@qq.com>
2018-11-09 23:53:29 +0800

    m

--- diff/baidu/ext.js
+++ diff/baidu/ext.js
@@ -5,6 +5,7 @@ module.exports = {
         'domainPathDev': 'http://thomas.ec-os.net/',
         'domainPathBuild': 'http://thomas.ec-os.net/',
         'apiToken': '8pdqc1lugsv0az2ipf4s57ssa7yghiwz',
+        "app_id": "14513230",
         'site_id': '438520',
         'api_version': '1.0',
         'api_type': 'baidu',
